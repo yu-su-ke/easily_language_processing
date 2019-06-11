08a0cee47e112f669c8537e32bad8399749770d3
yusuke <mikaduki.2448@gmail.com>
2019-06-12 01:24:45 +0900

    update main_oseti.py

--- pn/main_oseti.py
+++ pn/main_oseti.py
@@ -3,4 +3,12 @@ import oseti
 file_name = 'test'
 with open('./document/' + file_name + '.txt', mode='r', encoding="utf-8") as text_file:
     analyzer = oseti.Analyzer()
-    print(analyzer.analyze(text_file.read()))
+    tmp = analyzer.analyze(text_file.read())
+    print(tmp)
+    print(sum(tmp) / len(tmp))
+
+file_name2 = 'doc1'
+with open ('./document/' + file_name2 + '.txt', mode='r', encoding='utf-8') as text_file2:
+    analyzer = oseti.Analyzer()
+    tmp2 = analyzer.analyze(text_file2.read())
+    print(tmp2)
