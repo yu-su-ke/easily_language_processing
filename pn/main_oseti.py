9be9a8e96c922d4e966e3284f1fd021e845d1394
yu-su-ke <mikaduki.2448@gmail.com>
2019-06-12 17:53:19 +0900

    update

--- pn/main_oseti.py
+++ pn/main_oseti.py
@@ -6,9 +6,3 @@ with open('./document/' + file_name + '.txt', mode='r', encoding="utf-8") as tex
     tmp = analyzer.analyze(text_file.read())
     print(tmp)
     print(sum(tmp) / len(tmp))
-
-file_name2 = 'doc1'
-with open('./document/' + file_name2 + '.txt', mode='r', encoding='utf-8') as text_file2:
-    analyzer = oseti.Analyzer()
-    tmp2 = analyzer.analyze(text_file2.read())
-    print(tmp2)
