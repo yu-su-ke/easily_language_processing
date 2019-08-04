4fe1c2b9eca75d9c574235e4c5dabcfe38e584ee
yu-su-ke <mikaduki.2448@gmail.com>
2019-08-04 23:21:03 +0900

    update

--- test/test_main.py
+++ test/test_main.py
@@ -2,5 +2,6 @@ import MeCab
 m = MeCab.Tagger(' -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
 
 text = '''
-中澤先生、教授へのご就任、誠におめでとうございます。先生のこれまでの研究の成果が認められ、私もとても嬉しく、心より感激しています。今後とも変わらぬご指導・ご鞭撻をいただきますよう何卒宜しくお願い申し上げます。また、くれぐれもご無理などなさらないようご自愛くださいませ。'''
+テスト
+'''
 print(m.parse(text))
