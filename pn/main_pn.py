1f1001997f8463b891ac483c6c1202aea2a14a5a
yu-su-ke <mikaduki.2448@gmail.com>
2019-12-16 14:47:01 +0900

    update

--- pn/main_pn.py
+++
@@ -1,6 +0,0 @@
-from pn_document import pn_tweets
-
-file_name = 'test'
-with open('./document/' + file_name + '.txt', mode='r', encoding="utf-8") as text_file:
-    average, pn_sum = pn_tweets(text_file.read())
-    print('平均 : ' + average + '合計 : ' + pn_sum)
